15d8c597a23885f0788f538922f6daadd8b57cf0
nextlevelshit <mail@dailysh.it>
2016-04-17 14:36:04 +0200

    Configured test workflow

---
+++ test/first.spec.js
@@ -0,0 +1,11 @@
+'use strict';
+
+describe('Just checking if everything is loaded', function() {
+
+    it('Checking console', function () {
+        var test = true;
+
+        expect(test).toEqual(true);
+    });
+
+});
